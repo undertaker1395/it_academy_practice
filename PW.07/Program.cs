0bbf9dbb28c993b7df9563c6ca8351e747bb9d17
lfid95 <lfid95@gmail.com>
2021-02-22 21:03:21 +0300

    Added Task 3

--- PW.07/Program.cs
+++ PW.07/Program.cs
@@ -14,5 +14,27 @@ namespace PW._07
             string[] emptyArr = new string[0];
             return emptyArr;
         }
+
+        public string[] TriplArray(string[] arr)
+        {
+            int num = 32;
+            char leter = 'a';
+            string word = "Hello!";
+            SomeType[] triplArr = new string[] {num, leter, word };
+            return triplArr;
+        }
+
+        public int MaxArrVal(int max)
+        {
+            int[] maxValArr = new int[13];
+            int maxVal = 0;
+            Random rnd = new Random();
+            for (int i = 0; i < maxValArr.Length; i++)
+            {
+                maxValArr[i] = rnd.Next(maxVal, 10);
+                if (maxValArr[i] > maxVal) maxVal = maxValArr[i];
+            }
+            return maxVal;
+        }
     }
 }
