eca90bc33df82d9122716ef4eaa168bc57244c34
lfid95 <lfid95@gmail.com>
2021-02-22 20:39:37 +0300

    Added Task 1

---
+++ PW.07/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PW._07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public string[] EmptyArray(string[] arr)
+        {
+            string[] emptyArr = new string[0];
+            return emptyArr;
+        }
+    }
+}
