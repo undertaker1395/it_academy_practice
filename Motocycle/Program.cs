b50ad03cfb933c5dbc5c980acfb59cb294cbfd5e
lfid95 <lfid95@gmail.com>
2021-03-10 20:05:37 +0300

    Trying motocycle

---
+++ Motocycle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Motocycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Motocycle
+    {
+        public Motocycle(string mod, string prod, int mil)
+        {
+            Guid serialNum = Guid.NewGuid();
+            string model = mod;
+            string producer = prod;
+            int issueYear = DateTime.Now.Year;
+            int milage = mil;
+        }
+        
+       
+
+        private void Reset()
+        {
+            Motocycle.
+        }
+
+        class Engine
+        {
+            int volume = 0;
+            int power = 0;
+        }
+    }
+}
