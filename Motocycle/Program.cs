638ec0c568cd72867ba3b370a649186878006a4c
lfid95 <lfid95@gmail.com>
2021-03-13 13:52:10 +0300

    fixed

--- Motocycle/Program.cs
+++ Motocycle/Program.cs
@@ -4,34 +4,62 @@ namespace Motocycle
 {
     class Program
     {
+        static Motocycle[] motocycles;
+        
+        static void PrintMoto(Motocycle motocycle) 
+        {
+            Console.WriteLine("{6}. Мотоцикл(Производитель): {0}, Модель: {1}, Идентификатор: {2}, Год: {3}\n{6}.1 Двигатель(Объём): {4}, Мощьность: {5}",
+                motocycle.producer, motocycle.model, motocycle.serialNum, motocycle.issueYear, motocycle.Engine.volume, motocycle.Engine.power, motocycle.num)
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            motocycles = new Motocycle[3];
+            foreach(int m in motocycles)
+            {
+                motocycles[m] = new Motocycle();
+                motocycles[m].num = m + 1;
+                readonly motocycles[m].serialNum = new Guid();
+                motocycles[m].issueYear = DateTime.Now.Year;
+                Console.WriteLine("Укажите модель мотоцикла");
+                motocycles[m].model = Console.ReadLine();
+                Console.WriteLine("Укажите производителя мотоцикла");
+                motocycles[m].producer = Console.ReadLine();
+                Console.WriteLine("Укажите пробег мотоцикла");
+                motocycles[m].milage = Console.ReadLine();
+                Console.WriteLine("Укажите объём двигателя");
+                motocycles[m].Engine.volume = Console.ReadLine();
+                Console.WriteLine("Укажите мощность двигателя");
+                motocycles[m].Engine.power = Console.ReadLine();
+            }
+
+            PrintMoto(motocycles[0]);
+            PrintMoto(motocycles[1]);
+            PrintMoto(motocycles[2]);
         }
     }
 
     class Motocycle
     {
-        public Motocycle(string mod, string prod, int mil)
-        {
-            Guid serialNum = Guid.NewGuid();
-            string model = mod;
-            string producer = prod;
-            int issueYear = DateTime.Now.Year;
-            int milage = mil;
-        }
+        internal Guid serialNum;
+        internal string model;
+        internal string producer;
+        internal int issueYear = DateTime.Now.Year;
+        internal int milage;
+        internal int num;
+    
         
        
 
         private void Reset()
         {
-            Motocycle.
+            
         }
 
-        class Engine
+        internal class Engine
         {
-            int volume = 0;
-            int power = 0;
+            internal int volume;
+            internal int power;
         }
     }
 }
