0945e9be1c771bb585a358b89afaa5f0ecd2ca9d
lfid95 <lfid95@gmail.com>
2021-03-17 12:17:05 +0300

    fixed

--- Motocycle/Program.cs
+++ Motocycle/Program.cs
@@ -5,24 +5,22 @@ namespace Motocycle
     class Program
     {
         static Motocycle[] motocycles;
-        static Engine[] engines;
+        static Motocycle.Engine[] engines;
         
-
-        static void PrintMoto(Motocycle motocycle)
+        static void PrintMoto(Motocycle motocycle, Motocycle.Engine engine)
         {
             Console.WriteLine("{6}. Мотоцикл(Производитель): {0}, Модель: {1}, Идентификатор: {2}, Год: {3}\n{6}.1 Двигатель(Объём): {4}, Мощьность: {5}",
-                motocycle.producer, motocycle.model, motocycle.serialNum, motocycle.issueYear, motocycle.engine.volume, motocycle.engine.power, motocycle.num);
+                motocycle.producer, motocycle.model, motocycle.serialNum, motocycle.issueYear, engine.volume, engine.power, motocycle.num);
         }
 
         static void Main(string[] args)
         {
             motocycles = new Motocycle[3];
-            engines = new Engine[3];
+            engines = new Motocycle.Engine[3];
             
             for (int m = 0; m < 3;  m++)
             {
                 motocycles[m] = new Motocycle();
-                motocycles[m].engine = new Engine();
                 motocycles[m].num = m + 1;
                 motocycles[m].serialNum = Guid.NewGuid();
                 motocycles[m].issueYear = DateTime.Now.Year;
@@ -33,37 +31,80 @@ namespace Motocycle
                 Console.WriteLine("Укажите пробег мотоцикла");
                 motocycles[m].milage = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Укажите объём двигателя");
-                motocycles[m].engine.volume = Convert.ToInt32(Console.ReadLine());
+                engines[m].volume = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Укажите мощность двигателя");
-                motocycles[m].engine.power = Convert.ToInt32(Console.ReadLine());
+                engines[m].power = Convert.ToInt32(Console.ReadLine());
             }
-            for (int i = 0; i < 3; i++) PrintMoto(motocycles[i]);
-           
+            for (int i = 0; i < 3; i++) PrintMoto(motocycles[i], engines[i]);
         }
     }
 
     class Motocycle
     {
-        internal Guid serialNum;
-        internal string model;
-        internal string producer;
-        internal int issueYear = DateTime.Now.Year;
-        internal int milage;
+        readonly Guid _serialNum;
+        string _model;
+        string _producer;
+        int _issueYear;
+        int _milage;
         internal int num;
-        internal Engine engine;
-    
-        
-       
+
+        public Guid serialNum { get { return _serialNum; } set { } }
+        public string model { get { return _model; } set { } }
+        public string producer { get { return _producer; } set { } }
+        public int issueYear { get { return _issueYear; } set { } }
+        public int milage 
+        { 
+            get 
+            { 
+                return _milage; 
+            }
+            set 
+            {
+                if (value > 100)
+                    _milage = 100;
+                else
+                    _milage = value;
+            }
+        }
 
         private void Reset()
         {
             
         }
 
-    }
-    class Engine
-    {
-        internal int volume;
-        internal int power;
-    }
+        internal class Engine
+        {
+            int _volume;
+            int _power;
+
+            public int volume 
+            {
+                get { return _volume; }
+
+                set 
+                {
+                    if (value < 125)
+                        _volume = 125;
+                    else if (value > 3200)
+                        _volume = 3200;
+                    else
+                        _volume = value;
+                }
+            }
+            public int power
+            {
+                get { return _volume; }
+
+                set
+                {
+                    if (value < 50)
+                        _power = 50;
+                    else if (value > 300)
+                        _power = 300;
+                    else
+                        _power = value;
+                }
+            }
+        }
+    } 
 }
