7a8f644783e0d9f2177f1a320dd1280ecda1f5c1
lfid95 <lfid95@gmail.com>
2021-04-07 17:30:49 +0300

    Added shortString project

---
+++ lecture_16/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lecture_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello World!";
+            string result = str.ShortString(str);
+            Console.WriteLine(result);
+        }
+    }
+
+    public static class StringExtension
+    {
+        public static string ShortString(this string str, string longString)
+        {
+            string shortString = longString.Substring(0, 4) + "...";
+            return shortString;
+        }
+    }
+}
